307f21245d9d2452559e0def3c7125e844b2b3ea
Denis Katkov <Denis.Katkov@standardbank.co.za>
2021-11-18 21:47:01 +0300

    simple sql query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<()> {
+    let conn = Connection::open("/Users/dremax/portfolio/sqlite/example.db")?;
+
+    conn.execute(
+        "create table if not exists photos (
+                id integer primary key,
+                path text not null
+        )", [])?;
+    
+    Ok(())
 }
