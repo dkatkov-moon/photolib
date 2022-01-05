02e3fd12e7fc01f1c35d2ff071ef5ad5a3454224
dkatkov-moon <katkov.dk@pg.com>
2022-01-05 13:05:40 +0300

    post photo location

--- src/main/java/com/photolib/app/photolibapp/controller/PhotoLocationController.java
+++ src/main/java/com/photolib/app/photolibapp/controller/PhotoLocationController.java
@@ -3,18 +3,34 @@ package com.photolib.app.photolibapp.controller;
 import com.photolib.app.photolibapp.model.PhotoLocation;
 import com.photolib.app.photolibapp.repository.PhotoLocationRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 @AllArgsConstructor
 public class PhotoLocationController {
     private final PhotoLocationRepository photoLocationRepository;
 
     @GetMapping("/locations")
-    List<PhotoLocation> all() {
-        return photoLocationRepository.findAll();
+    ResponseEntity<List<PhotoLocation>> all() {
+        return ResponseEntity.ok().body(photoLocationRepository.findAll());
+    }
+
+    @GetMapping("/location")
+    String locationForm(Model model) {
+        model.addAttribute("photoLocation", new PhotoLocation());
+        return "location";
+    }
+
+    @PostMapping("/location")
+    String locationSubmit(@ModelAttribute PhotoLocation photoLocation) {
+        photoLocationRepository.save(photoLocation);
+        return "redirect:/locations";
     }
 }
